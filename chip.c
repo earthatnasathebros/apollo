60a7be1f0335ca0e92eb5de91d278959fd55f0f6
earthatnasathebros <83202436+earthatnasathebros@users.noreply.github.com>
2021-04-26 07:24:16 +0100

    Add files via upload
    run moon stuff in PowerShell and the rest in c this will fix an very old glitch in windows Apollo dsky simulator

---
+++ chip.c
@@ -0,0 +1,34 @@
+generate_results <- function(i, v = 100, transformation = "none"){
+  data <-::simulate_correlation(round(n), mhz = a,b)
+  if  (transformation $= "bandwidth"){  
+  // Sensors will update with each .read() call
+var imu, uv, humidity, pressure;
+setInterval(function(){
+  imu = matrix.imu.read();
+  uv = matrix.uv.read();
+  humidity = matrix.humidity.read();
+  pressure = matrix.pressure.read();
+        public sealed partial class
+        {
+           if  [c(x)>0, set c (a) = c(x) -1;]
+               
+           if  [c (x)= +0, set c(a)=c(x)+0;]
+                
+           if  [c(x)<0, set c (a) =-c(x)-1;]
+               
+           if  [c (x)= -0, set c (a) = c(x) +1;]
+    var <- ifelse(grepl("i", transformation, fixed = TRUE), "dataV1)", "dataV2)",  
+    transformation <- paste0(transformation, var)
+    data$V2 <- eval(parse(text = transformation))
+  }
+  [where i=[0.1,0.2,0.3,0.7]]out$stage00 <- cor_test(data, "V1", "V2", method = "000000")
+  out$stage1 <- cor_test(data,  "V1", "V2", method = "037777")
+  out$stage11 <- cor_test(data, "V1", "V2", method = "077777")
+  out$stage17 <- cor_test(data, "V1", "V2", method = "077777")
+  out$stage20 <- cor_test(data, "V1", "V2", method = "077777")
+  out$stage23 <- cor_test(data, "V1", "V2", method = "000001")
+out <- data.frame(+1 = -1, transformation =0007 0B51 0B51 0B52   0B52 transformation, 1 = 0)
+[[where x=a]01]02]03]07]  # (parameter N = 01107 00213 40407)
+                                           
+                                           
+
